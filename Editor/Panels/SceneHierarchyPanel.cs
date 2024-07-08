49731a5b831e8231afbf25c6922a030fb72c4737
mateuszkulesza <matkul3@gmail.com>
2024-07-08 14:49:16 +0200

    layouting

--- Editor/Panels/SceneHierarchyPanel.cs
+++ Editor/Panels/SceneHierarchyPanel.cs
@@ -1,5 +1,7 @@
+using System.Numerics;
 using ECS;
 using Engine.Scene;
+using Engine.Scene.Components;
 using ImGuiNET;
 
 namespace Editor.Panels;
@@ -7,7 +9,7 @@ namespace Editor.Panels;
 public class SceneHierarchyPanel
 {
     private Scene _context;
-    private Entity _selectionContext;
+    private Entity? _selectionContext;
 
     public SceneHierarchyPanel(Scene context)
     {
@@ -27,10 +29,61 @@ public class SceneHierarchyPanel
         {
             DrawEntityNode(entity);
         }
-        
+
+        if (ImGui.IsMouseDown(0) && ImGui.IsWindowHovered())
+            _selectionContext = null;
+
+        ImGui.End();
+
+        ImGui.Begin("Properties");
+        if (_selectionContext is not null)
+            DrawComponents(_selectionContext);
+
         ImGui.End();
     }
-    
+
+    private void DrawComponents(Entity entity)
+    {
+        var tag = entity.Name;
+
+        byte[] buffer = new byte[256];
+        Array.Clear(buffer, 0, buffer.Length);
+        byte[] tagBytes = System.Text.Encoding.UTF8.GetBytes(tag);
+        Array.Copy(tagBytes, buffer, Math.Min(tagBytes.Length, buffer.Length - 1));
+
+        if (ImGui.InputText("Tag", buffer, (uint)buffer.Length))
+        {
+            // Convert byte[] buffer back to string
+            tag = System.Text.Encoding.UTF8.GetString(buffer).TrimEnd('\0');
+            entity.Name = tag; // Update entity's name if needed
+        }
+
+
+        if (entity.HasComponent<TransformComponent>())
+        {
+            // ImGui TreeNode for TransformComponent
+            if (ImGui.TreeNodeEx(typeof(TransformComponent).GetHashCode(), ImGuiTreeNodeFlags.DefaultOpen, "Transform"))
+            {
+                // Get reference to TransformComponent's Transform
+                var transform = entity.GetComponent<TransformComponent>().Transform;
+
+                // ImGui DragFloat3 for editing position
+                Vector3 position = new Vector3(transform.M41, transform.M42, transform.M43);
+                if (ImGui.DragFloat3("Position", ref position, 0.1f))
+                {
+                    // Update transform with new position values
+                    transform.M41 = position[0];
+                    transform.M42 = position[1];
+                    transform.M43 = position[2];
+                    // Update TransformComponent in entity if needed
+                    entity.GetComponent<TransformComponent>().Transform = transform;
+                }
+
+                ImGui.TreePop(); // Close TreeNode
+            }
+        }
+    }
+
     private void DrawEntityNode(Entity entity)
     {
         var tag = entity.Name;
@@ -50,6 +103,5 @@ public class SceneHierarchyPanel
                 ImGui.TreePop();
             ImGui.TreePop();
         }
-
     }
 }
--- Engine/ImGuiNet/ImGuiLayer.cs
+++ Engine/ImGuiNet/ImGuiLayer.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Engine.Core;
 using Engine.Events;
 using Engine.Platform.SilkNet;
@@ -66,5 +67,80 @@ public class ImGuiLayer : Layer
         io.ConfigFlags |= ImGuiConfigFlags.ViewportsEnable;
         io.ConfigFlags |= ImGuiConfigFlags.DockingEnable;
         io.WantSaveIniSettings = true;
+
+        io.Fonts.AddFontFromFileTTF("assets/fonts/opensans/OpenSans-Bold.ttf", 18.0f);
+        //io.FontDefault = io.Fonts.AddFontFromFileTTF("assets/fonts/opensans/OpenSans-Regular.ttf", 18.0f);
+
+        ImGui.StyleColorsDark();
+
+        var style = ImGui.GetStyle();
+        if ((io.ConfigFlags & ImGuiConfigFlags.ViewportsEnable) != 0)
+        {
+            style.WindowRounding = 0.0f;
+            style.Colors[(int)ImGuiCol.WindowBg].W = 1.0f;
+        }
+
+        SetDarkThemeColors();
+    }
+
+    private static void SetDarkThemeColors()
+    {
+        var colors = ImGui.GetStyle().Colors;
+        colors[(int)ImGuiCol.WindowBg] = new Vector4(
+            0.1f, 0.105f, 0.11f, 1.0f
+        );
+
+
+        // Headers
+        colors[(int)ImGuiCol.Header] = new Vector4(
+            0.2f, 0.205f, 0.21f, 1.0f);
+
+        colors[(int)ImGuiCol.HeaderHovered] = new Vector4(
+            0.3f, 0.305f, 0.31f, 1.0f);
+
+        colors[(int)ImGuiCol.HeaderActive] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f
+        );
+
+        // Buttons
+        colors[(int)ImGuiCol.Button] = new Vector4(
+            0.2f, 0.205f, 0.21f, 1.0f
+        );
+        colors[(int)ImGuiCol.ButtonHovered] = new Vector4(
+            0.3f, 0.305f, 0.31f, 1.0f
+        );
+        colors[(int)ImGuiCol.ButtonActive] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f
+        );
+
+        // Frame BG
+        colors[(int)ImGuiCol.FrameBg] = new Vector4(
+            0.2f, 0.205f, 0.21f, 1.0f
+        );
+        colors[(int)ImGuiCol.FrameBgHovered] = new Vector4(
+            0.3f, 0.305f, 0.31f, 1.0f
+        );
+        colors[(int)ImGuiCol.FrameBgActive] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f);
+
+        // Tabs
+        colors[(int)ImGuiCol.Tab] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f);
+        colors[(int)ImGuiCol.TabHovered] = new Vector4(
+            0.38f, 0.3805f, 0.381f, 1.0f);
+        colors[(int)ImGuiCol.TabActive] = new Vector4(
+            0.28f, 0.2805f, 0.281f, 1.0f);
+
+        colors[(int)ImGuiCol.TabUnfocused] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f);
+        colors[(int)ImGuiCol.TabUnfocusedActive] = new Vector4(
+            0.2f, 0.205f, 0.21f, 1.0f);
+        // Title
+        colors[(int)ImGuiCol.TitleBg] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f);
+        colors[(int)ImGuiCol.TitleBgActive] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f);
+        colors[(int)ImGuiCol.TitleBgCollapsed] = new Vector4(
+            0.15f, 0.1505f, 0.151f, 1.0f);
     }
 }
