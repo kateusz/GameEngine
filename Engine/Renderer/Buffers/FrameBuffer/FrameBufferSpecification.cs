58d925295c5b0e159cdfe1461d694cec9197749a
mateuszkulesza <matkul3@gmail.com>
2024-08-13 21:24:44 +0200

    move frame buffers to separate dir

--- Engine/Renderer/Buffers/FrameBuffer/FrameBufferSpecification.cs
+++ Engine/Renderer/Buffers/FrameBuffer/FrameBufferSpecification.cs
@@ -1,4 +1,4 @@
-namespace Engine.Renderer.Buffers;
+namespace Engine.Renderer.Buffers.FrameBuffer;
 
 public class FrameBufferSpecification(uint width, uint height, uint samples = 1, bool swapChainTarget = false)
 {
