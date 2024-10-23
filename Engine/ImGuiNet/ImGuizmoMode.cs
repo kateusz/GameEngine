51cf13698fce5404194b04cbb12c086fef1ea9bc
Mateusz Kulesza <matkul3@gmail.com>
2024-10-23 22:47:53 +0200

    cleanup

--- Engine/ImGuiNet/ImGuizmoMode.cs
+++
@@ -1,8 +0,0 @@
-namespace Engine.ImGuiNet;
-
-public enum ImGuizmoMode
-{
-    Local = 0,   // Local coordinate system
-    World = 1,   // World coordinate system
-    All = 2      // All coordinate systems (used for some special operations)
-}
