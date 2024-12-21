e1d93d5ee6e498aa91660cd29c24110e1f5d52c0
Mateusz Kulesza <matkul3@gmail.com>
2024-12-21 20:08:40 +0100

    Sandbox map of tiles

--- Sandbox/Sandbox2DLayer.cs
+++ Sandbox/Sandbox2DLayer.cs
@@ -11,29 +11,71 @@ namespace Sandbox;
 
 public class Sandbox2DLayer : Layer
 {
+    private const int _mapWidth = 24;
+    private const int _mapHeight = 24;
+
+    private const string _mapTiles =
+        """
+        WDWDWDWDWDWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWDDDDWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWDDDDWWWWW
+        WDWDWDWDWDWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWDDDDWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWDDDDWWWWW
+        WDWDWDWDWDWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWDDDDWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWDDDDWWWWW
+        WDWDWDWDWDWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        WWWWWWWDDDDWWWWWWWWWWWWW
+        WWWWWWWWWWWWWWWWWWWWWWWW
+        DWWWWWWWWWWWWWWDDDDWWWWW
+        """;
+
+
     private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
     private OrthographicCameraController _orthographicCameraController;
     private Texture2D _spriteSheet;
     private SubTexture2D _textureBarrel;
 
+    private readonly Dictionary<char, SubTexture2D> _textureMap = new();
+    private readonly char[,] _mapArray;
+
     public Sandbox2DLayer(string name) : base(name)
     {
+        _mapArray = ConvertMapTo2DArray(_mapTiles, _mapWidth, _mapHeight);
     }
-    
+
     public override void OnAttach()
     {
-        Logger.Debug("ExampleLayer OnAttach.");
+        Logger.Debug("Sandbox2DLayer OnAttach.");
 
-        _orthographicCameraController = new OrthographicCameraController(1280.0f / 720.0f, true);
+        _orthographicCameraController = new OrthographicCameraController(3840.0f / 2160.0f, true);
         _spriteSheet = TextureFactory.Create("assets/game/textures/RPGpack_sheet_2X.png");
-        
-        _textureBarrel = SubTexture2D.CreateFromCoords(_spriteSheet, new Vector2(8, 2), new Vector2(128, 128), new Vector2(1, 1));
+
+        _textureBarrel =
+            SubTexture2D.CreateFromCoords(_spriteSheet, new Vector2(8, 2), new Vector2(128, 128), new Vector2(1, 1));
+
+        _textureMap['D'] =
+            SubTexture2D.CreateFromCoords(_spriteSheet, new Vector2(6, 11), new Vector2(128, 128), new Vector2(1, 1));
+        _textureMap['W'] = SubTexture2D.CreateFromCoords(_spriteSheet, new Vector2(11, 11), new Vector2(128, 128),
+            new Vector2(1, 1));
     }
 
     public override void OnDetach()
     {
-        Logger.Debug("ExampleLayer OnDetach.");
+        Logger.Debug("Sandbox2DLayer OnDetach.");
     }
 
     public override void OnUpdate(TimeSpan timeSpan)
@@ -45,19 +87,18 @@ public class Sandbox2DLayer : Layer
 
         Renderer2D.Instance.BeginScene(_orthographicCameraController.Camera);
 
-        // sub texture
-        Renderer2D.Instance.DrawQuad(new Vector3(2.0f, 2.0f, 0.5f), new Vector2(1, 1), rotation: 0, _textureBarrel);
+        for (var row = 0; row < _mapHeight; row++)
+        {
+            for (var col = 0; col < _mapWidth; col++)
+            {
+                var subTextureCode = _mapArray[row, col];
+                var subTexture = _textureMap[subTextureCode];
+                
+                // _mapHeight - row because openGl reads from bottom left to top right
+                Renderer2D.Instance.DrawQuad(new Vector3(col - _mapWidth / 2.0f, _mapHeight - row / 2.0f, 0.5f), new Vector2(1, 1), rotation: 0, subTexture);
+            }
+        }
 
-        // yellow
-        Renderer2D.Instance.DrawQuad(
-            new Vector3(0.0f, 0.0f, 0.0f),
-            new Vector2(1.0f, 1.0f),
-            new Vector4(0.8f, 0.8f, 0.3f, 1.0f));
-        
-        // red
-        Renderer2D.Instance.DrawQuad(new Vector2(-1.0f, 0.0f), new Vector2(0.8f, 0.8f),
-            new Vector4(0.8f, 0.2f, 0.3f, 1.0f));
-        
         Renderer2D.Instance.EndScene();
     }
 
@@ -70,11 +111,21 @@ public class Sandbox2DLayer : Layer
 
     public override void OnImGuiRender()
     {
-        SubmitUI();
     }
-    
-    private void SubmitUI()
+
+    static char[,] ConvertMapTo2DArray(string mapTiles, int mapWidth, int mapHeight)
     {
-        //ImGui.ShowDemoWindow();
+        char[,] mapArray = new char[mapHeight, mapWidth];
+        string[] rows = mapTiles.Trim().Split('\n');
+
+        for (int row = 0; row < mapHeight; row++)
+        {
+            for (int col = 0; col < mapWidth; col++)
+            {
+                mapArray[row, col] = rows[row][col];
+            }
+        }
+
+        return mapArray;
     }
 }
