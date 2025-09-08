78c402c351a4cac7111e3da6d14f34b36b72794e
Mateusz Kulesza <matkul3@gmail.com>
2025-09-08 04:42:56 +0200

    IoC

--- Editor/EditorModule.cs
+++ Editor/EditorModule.cs
@@ -1,4 +1,6 @@
 using DryIoc;
+using Editor.Managers;
+using Editor.Panels;
 using Engine.Core;
 
 namespace Editor;
@@ -10,5 +12,14 @@ public class EditorModule : IModule
     public void Register(IContainer container)
     {
         container.Register<EditorLayer>(Reuse.Singleton);
+        container.Register<SceneManager>(Reuse.Singleton);
+        container.Register<ContentBrowserPanel>(Reuse.Singleton);
+        container.Register<ConsolePanel>(Reuse.Singleton);
+        container.Register<PropertiesPanel>(Reuse.Singleton);
+        container.Register<ProjectManager>(Reuse.Singleton);
+        container.Register<ProjectUI>(Reuse.Singleton);
+        container.Register<EditorToolbar>(Reuse.Singleton);
+        container.Register<EditorSettingsUI>(Reuse.Singleton);
+        container.Register<EditorSettings>(Reuse.Singleton);
     }
 }
