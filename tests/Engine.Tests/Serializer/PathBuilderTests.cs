5e2f82539e29e18439b1c4af289b798e3e61bce1
kateusz <matkul3@gmail.com>
2026-07-08 23:48:14 +0200

    fix: make PathBuilderTests Linux-safe, add TRX artifacts on CI failure

--- tests/Engine.Tests/Serializer/PathBuilderTests.cs
+++ tests/Engine.Tests/Serializer/PathBuilderTests.cs
@@ -6,82 +6,91 @@ namespace Engine.Tests.Serializer;
 
 public class PathBuilderTests : IDisposable
 {
-    public PathBuilderTests()
-    {
-        PathBuilder.UseProjectContext(Substitute.For<IProjectContext>());
-    }
+  private static string GameAssets =>
+    OperatingSystem.IsWindows() ? @"C:\game\assets" : "/game/assets";
 
-    public void Dispose()
-    {
-        PathBuilder.UseProjectContext(Substitute.For<IProjectContext>());
-    }
+  private static string RootedTexturePath =>
+    OperatingSystem.IsWindows() ? @"C:\game\assets\texture.png" : "/game/assets/texture.png";
 
-    [Fact]
-    public void AssetsPath_before_initialization_throws()
-    {
-        PathBuilder.UseProjectContext(null!);
+  private static string AssetsPrefixedRelativePath =>
+    OperatingSystem.IsWindows() ? @"assets\textures\player.png" : "assets/textures/player.png";
 
-        var ex = Should.Throw<InvalidOperationException>(() => _ = PathBuilder.AssetsPath);
-        ex.Message.ShouldContain("not initialized");
-    }
+  public PathBuilderTests()
+  {
+    PathBuilder.UseProjectContext(Substitute.For<IProjectContext>());
+  }
 
-    [Fact]
-    public void AssetsPath_after_initialization_returns_context_path()
-    {
-        var context = Substitute.For<IProjectContext>();
-        context.AssetsPath.Returns(@"C:\game\assets");
-        PathBuilder.UseProjectContext(context);
+  public void Dispose()
+  {
+    PathBuilder.UseProjectContext(Substitute.For<IProjectContext>());
+  }
 
-        PathBuilder.AssetsPath.ShouldBe(@"C:\game\assets");
-    }
+  [Fact]
+  public void AssetsPath_before_initialization_throws()
+  {
+    PathBuilder.UseProjectContext(null!);
 
-    [Fact]
-    public void Resolve_null_or_whitespace_returns_input()
-    {
-        PathBuilder.Resolve(null!).ShouldBeNull();
-        PathBuilder.Resolve("").ShouldBe("");
-        PathBuilder.Resolve("   ").ShouldBe("   ");
-    }
+    var ex = Should.Throw<InvalidOperationException>(() => _ = PathBuilder.AssetsPath);
+    ex.Message.ShouldContain("not initialized");
+  }
 
-    [Fact]
-    public void Resolve_rooted_path_returns_normalized()
-    {
-        var result = PathBuilder.Resolve(@"C:\game\assets\texture.png");
-        result.ShouldBe(@"C:\game\assets\texture.png");
-    }
+  [Fact]
+  public void AssetsPath_after_initialization_returns_context_path()
+  {
+    var context = Substitute.For<IProjectContext>();
+    context.AssetsPath.Returns(GameAssets);
+    PathBuilder.UseProjectContext(context);
 
-    [Fact]
-    public void Resolve_relative_path_combines_with_assets()
-    {
-        var context = Substitute.For<IProjectContext>();
-        context.AssetsPath.Returns(@"C:\game\assets");
-        PathBuilder.UseProjectContext(context);
+    PathBuilder.AssetsPath.ShouldBe(GameAssets);
+  }
 
-        var result = PathBuilder.Resolve(@"textures/player.png");
+  [Fact]
+  public void Resolve_null_or_whitespace_returns_input()
+  {
+    PathBuilder.Resolve(null!).ShouldBeNull();
+    PathBuilder.Resolve("").ShouldBe("");
+    PathBuilder.Resolve("   ").ShouldBe("   ");
+  }
 
-        result.ShouldBe(Path.GetFullPath(Path.Combine(@"C:\game\assets", @"textures/player.png")));
-    }
+  [Fact]
+  public void Resolve_rooted_path_returns_normalized()
+  {
+    var result = PathBuilder.Resolve(RootedTexturePath);
+    result.ShouldBe(RootedTexturePath);
+  }
 
-    [Fact]
-    public void Resolve_assets_prefix_stripped()
-    {
-        var context = Substitute.For<IProjectContext>();
-        context.AssetsPath.Returns(@"C:\game\assets");
-        PathBuilder.UseProjectContext(context);
+  [Fact]
+  public void Resolve_relative_path_combines_with_assets()
+  {
+    var context = Substitute.For<IProjectContext>();
+    context.AssetsPath.Returns(GameAssets);
+    PathBuilder.UseProjectContext(context);
 
-        var result = PathBuilder.Resolve(@"assets\textures\player.png");
+    var result = PathBuilder.Resolve("textures/player.png");
 
-        var expected = Path.GetFullPath(Path.Combine(@"C:\game\assets", @"textures\player.png"));
-        result.ShouldBe(expected);
-    }
+    result.ShouldBe(Path.GetFullPath(Path.Combine(GameAssets, "textures/player.png")));
+  }
 
-    [Fact]
-    public void Build_delegates_to_Resolve()
-    {
-        var context = Substitute.For<IProjectContext>();
-        context.AssetsPath.Returns(@"C:\game\assets");
-        PathBuilder.UseProjectContext(context);
+  [Fact]
+  public void Resolve_assets_prefix_stripped()
+  {
+    var context = Substitute.For<IProjectContext>();
+    context.AssetsPath.Returns(GameAssets);
+    PathBuilder.UseProjectContext(context);
 
-        PathBuilder.Build(@"sprites/icon.png").ShouldBe(PathBuilder.Resolve(@"sprites/icon.png"));
-    }
+    var result = PathBuilder.Resolve(AssetsPrefixedRelativePath);
+
+    var expected = Path.GetFullPath(Path.Combine(GameAssets, "textures/player.png"));
+    result.ShouldBe(expected);
+  }
+
+  [Fact]
+  public void Build_delegates_to_Resolve()
+  {
+    var context = Substitute.For<IProjectContext>();
+    context.AssetsPath.Returns(GameAssets);
+    PathBuilder.UseProjectContext(context);
+
+    PathBuilder.Build("sprites/icon.png").ShouldBe(PathBuilder.Resolve("sprites/icon.png"));
+  }
 }
